50a342503aa6a8d07edcb4e5ffddb4cd04fdaf73
Sanrase <klimenko.aro@students.dvfu.ru>
2022-12-28 11:45:05 +0300

    print film

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ void push(struct List *list, struct Film *film){
     }
 }
 
-struct Film addFilmList(struct List *list) {
+void addFilmList(struct List *list) {
     FILE *films = fopen("films.txt", "r");
     char c = 'c';
     int flag = 0;
@@ -100,9 +100,124 @@ struct Film addFilmList(struct List *list) {
     }
 }
 
-void print(struct List list){
-    while
-    printf("%s\n%s\n%s\n%s\n%s", list.head->film.title, list.head->film.year, list.head->film.country, list.head->film.genre, list.head->film.rating);
+void loopList(struct List *list){
+    list->head->back = list->tail;
+    list->tail->next = list->head;
+}
+
+int len(const char *string){
+    int len = 0;
+    for(int i = 0; string[i] != '\0'; i++, len++);
+
+    return len;
+}
+
+void print(struct Film film){
+    char up[67];
+    char mid[67];
+    char down[67];
+    up[0] = (char)218;
+    up[65] = (char)191;
+    up[66] = '\0';
+    mid[0] = (char)179;
+    mid[65] = (char)179;
+    mid[66] = '\0';
+    down[0] = (char)192;
+    down[65] = (char)217;
+    down[66] ='\0';
+
+    for(int i = 1; i < 65; i++){
+        up[i] = (char)196;
+        down[i] = (char)196;
+        mid[i] = (char)32;
+    }
+
+    char title[67];
+    int middle, length = len(film.title);
+    middle = (67 - length) / 2;
+    title[0] = (char)179;
+    title[65] = (char)179;
+    title[66] = '\0';
+
+    for(int i = 1; i < middle; i++){
+        title[i] = (char)32;
+    }
+    for(int i = 0; i < length; i++){
+        title[middle + i] = film.title[i];
+    }
+    for(int i = middle + length; i < 65; i++){
+        title[i] = (char)32;
+    }
+
+    char year[67];
+    length = len(film.year);
+    middle = (67 - length) / 2;
+    year[0] = (char)179;
+    year[65] = (char)179;
+    year[66] = '\0';
+
+    for(int i = 1; i < middle; i++){
+        year[i] = (char)32;
+    }
+    for(int i = 0; i < length; i++){
+        year[middle + i] = film.year[i];
+    }
+    for(int i = middle + length; i < 65; i++){
+        year[i] = (char)32;
+    }
+
+    char country[67];
+    length = len(film.country);
+    middle = (67 - length) / 2;
+    country[0] = (char)179;
+    country[65] = (char)179;
+    country[66] = '\0';
+
+    for(int i = 1; i < middle; i++){
+        country[i] = (char)32;
+    }
+    for(int i = 0; i < length; i++){
+        country[middle + i] = film.country[i];
+    }
+    for(int i = middle + length; i < 65; i++){
+        country[i] = (char)32;
+    }
+
+    char genre[67];
+    length = len(film.genre);
+    middle = (67 - length) / 2;
+    genre[0] = (char)179;
+    genre[65] = (char)179;
+    genre[66] = '\0';
+
+    for(int i = 1; i < middle; i++){
+        genre[i] = (char)32;
+    }
+    for(int i = 0; i < length; i++){
+        genre[middle + i] = film.genre[i];
+    }
+    for(int i = middle + length; i < 65; i++){
+        genre[i] = (char)32;
+    }
+
+    char rating[67];
+    length = len(film.rating);
+    middle = (67 - length) / 2;
+    rating[0] = (char)179;
+    rating[65] = (char)179;
+    rating[66] = '\0';
+
+    for(int i = 1; i < middle; i++){
+        rating[i] = (char)32;
+    }
+    for(int i = 0; i < length; i++){
+        rating[middle + i] = film.rating[i];
+    }
+    for(int i = middle + length; i < 65; i++){
+        rating[i] = (char)32;
+    }
+
+    printf("\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s", up, mid, title, year, country, genre, rating, mid, down);
 }
 
 int main() {
@@ -111,7 +226,14 @@ int main() {
     struct List list;
     init(&list);
     addFilmList(&list);
-    print(list);
+    loopList(&list);
+    while(1){
+        struct Film *film;
+        struct List *cur;
+
+
+    }
+
 
     return 0;
 }
