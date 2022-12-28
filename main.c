95cde1719ccc52a81bb2badea465db722cfc506b
Sanrase <klimenko.aro@students.dvfu.ru>
2022-12-28 07:52:00 +0300

    read file.txt

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ struct Film{
 
 struct Node {
     int flag;
-    struct Film *film;
+    struct Film film;
     struct Node *next;
     struct Node *back;
 };
@@ -33,69 +33,84 @@ void init(struct List *list){
 
 void push(struct List *list, struct Film *film){
     if(list->tail->flag == 1 && list->head->flag == 1){
-        list->head->film = film;
+        list->head->film = *film;
         list->head->flag = 0;
     }else{
         struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
-        temp->film = film;
+        temp->film = *film;
+        temp->flag = 0;
+        list->tail->next = temp;
         temp->back = list->tail;
-        temp->next = list->head;
-        list->head->back = temp;
+        temp->next = NULL;
+        list->tail = temp;
     }
 }
 
 struct Film addFilmList(struct List *list) {
     FILE *films = fopen("films.txt", "r");
     char c = 'c';
+    int flag = 0;
 
-    while (c != '\0') {
+    while (c != '\377') {
         c = fgetc(films);
+        if(flag == 1){
+            c = fgetc(films);
+        }
         struct Film *film = (struct Film *) malloc(sizeof(struct Film));
-        while (c != '#' || c != '\0') {
+        int j  = 0;
+        while (c != '#' && c != '\377') {
+            flag = 1;
             for (int i = 0; c != '\n'; i++) {
                 film->title[i] = c;
                 c = fgetc(films);
+                j = i;
             }
-            c = fgetc(films)
+            film->title[j + 1] = '\0';
+            c = fgetc(films);
             for (int i = 0; c != '\n'; i++){
                 film->year[i] = c;
                 c = fgetc(films);
+                j = i;
             }
+            film->year[j + 1] = '\0';
             c = fgetc(films);
             for(int i = 0; c != '\n'; i++){
                 film->country[i] = c;
                 c = fgetc(films);
+                j = i;
             }
+            film->country[j + 1] = '\0';
             c = fgetc(films);
             for(int i = 0; c != '\n'; i++){
                 film->genre[i] = c;
                 c = fgetc(films);
+                j = i;
             }
+            film->genre[j + 1] = '\0';
             c = fgetc(films);
             for(int i = 0; c != '\n'; i++){
                 film->rating[i] = c;
                 c = fgetc(films);
+                j = i;
             }
             c = fgetc(films);
+            film->rating[j + 1] = '\0';
             push(list, film);
         }
     }
 }
 
 void print(struct List list){
-    print("%s\n%s\n%s\n%s\n%s", list.head->film->title,
-          list.head->film->year,
-          list.head->film->country,
-          list.head->film->genre,
-          list.head->film->rating)
+    while
+    printf("%s\n%s\n%s\n%s\n%s", list.head->film.title, list.head->film.year, list.head->film.country, list.head->film.genre, list.head->film.rating);
 }
 
 int main() {
     FILE *input;
 
-    struct List *list;
-    init(list);
-    addFilmList(list);
+    struct List list;
+    init(&list);
+    addFilmList(&list);
     print(list);
 
     return 0;
